d55a2d744d83017ca1b022afe2a139eda836e558
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2019-09-12 18:38:05 -0500

    Update ClienteDTO.java

--- RestauranteSoporte/src/co/com/restaurante/DTO/ClienteDTO.java
+++ RestauranteSoporte/src/co/com/restaurante/DTO/ClienteDTO.java
@@ -9,20 +9,22 @@ public class ClienteDTO extends UsuarioDTO
 	/**constructor 1 de clase ClienteDTO SIN CODIGO
 	 * @param correo
 	 * @param contraseña
+	 * @param estado
 	 */
-	public ClienteDTO(String correo, String contraseña)
+	public ClienteDTO(String correo, String contraseña, int estado)
 	{
-		super(correo, contraseña);
+		super(correo, contraseña, estado);
 	}
 	
 	/**constructor 2 de la clase ClienteDTO CON CODIGO
 	 * @param codigo
 	 * @param correo
 	 * @param contraceña
+	 *  @param estado
 	 */
-	public ClienteDTO(int codigo, String correo, String contraceña)
+	public ClienteDTO(int codigo, String correo, String contraceña, int estado)
 	{
-		super(correo, contraceña);
+		super(correo, contraceña, estado);
 		this.codigo = codigo;
 	}
 }
