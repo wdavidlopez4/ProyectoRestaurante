58f18b6d458cd07f67025fc249681cb516deafa4
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2019-09-12 18:38:15 -0500

    Update UsuarioDTO.java

--- RestauranteSoporte/src/co/com/restaurante/DTO/UsuarioDTO.java
+++ RestauranteSoporte/src/co/com/restaurante/DTO/UsuarioDTO.java
@@ -7,6 +7,16 @@ package co.com.restaurante.DTO;
 public abstract class UsuarioDTO 
 {
 	/**
+	 * constante que representa es estado habilitado 
+	 */
+	public static final int ESTADO_HABILITADO = 1;
+	
+	/**
+	 * constante que representa es estado inhabilitado 
+	 */
+	public static final int ESTADO_INHABILITADO = 0;
+	
+	/**
 	 * atributo que representa el codigo del usuario PK
 	 */
 	protected int codigo;
@@ -22,12 +32,18 @@ public abstract class UsuarioDTO
 	protected String contraseña;
 	
 	/**
+	 * atributo que representa el estado
+	 */
+	protected int estado;
+	
+	/**
 	 * contructor
 	 */
-	public UsuarioDTO(String correo, String contraseña)
+	public UsuarioDTO(String correo, String contraseña, int estado)
 	{
 		this.correo = correo;
 		this.contraseña = contraseña;
+		this.estado = estado;
 	}
 
 	/**
@@ -70,6 +86,20 @@ public abstract class UsuarioDTO
 	 */
 	public void setContraseña(String contraseña) {
 		this.contraseña = contraseña;
+	}
+	
+	/**
+	 * @return estado
+	 */
+	public int getEstado() {
+		return estado;
+	}
+
+	/**
+	 * @param estado
+	 */
+	public void setEstado(int estado) {
+		this.estado = estado;
 	}
 	
 }
