fe5b041f34cfdbf109027e36c264ed38cd4b6cae
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2019-09-12 16:25:08 -0500

    Update IDAO.java

--- restauranteDAL/src/co/com/restaurante/DAO/IDAO.java
+++ restauranteDAL/src/co/com/restaurante/DAO/IDAO.java
@@ -1,41 +1,18 @@
 package co.com.restaurante.DAO;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface IDAO<T>
 {
-	/**
-	 * metodo crear, crea el objeto lo inserta en la db
-	 * @param objeto
-	 * @return boolean True si se creo
-	 */
-	public boolean crear(T objeto)throws SQLException;
+	public boolean crear(T objeto);
 	
-	/**
-	 * metodo modificar, modifica el objeto, modifica la info en la db
-	 * @param objeto
-	 * @return boolean true se modifico
-	 */
-	public boolean modificar(T objeto)throws SQLException;
+	public boolean modificar(T objeto);
 	
-	/**
-	 * metodo eliminar, elimina el objeto, elimina la info de la db
-	 * metodo
-	 * @param codigo
-	 * @return  boolean true si se elimino
-	 */
-	public boolean eliminar(int codigo)throws SQLException;
+	public boolean eliminar(int codigo);
 	
-	/**
-	 * metodo buscarPorCodigo, arma el objeto desde la db
-	 * @param codigo
-	 * @return objeto T generics
-	 */
-	public T buscarPorCodigo(int codigo)throws SQLException;
+	public T buscarUnoObjeto(int codigo);
 	
-	/**metodo trae lista de objetos, arma la lista de objeto desde la db
-	 * @return
-	 */
-	public ArrayList<T> traerLista()throws SQLException;
+	public ArrayList<T> traerLista();
+	
+	public boolean desabilitar(int codigo);
 }
