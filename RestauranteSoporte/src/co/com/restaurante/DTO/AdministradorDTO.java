d39465f6e944ffdd6a58f22bb00e860abc107b4f
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2019-09-12 18:37:57 -0500

    Update AdministradorDTO.java

--- RestauranteSoporte/src/co/com/restaurante/DTO/AdministradorDTO.java
+++ RestauranteSoporte/src/co/com/restaurante/DTO/AdministradorDTO.java
@@ -9,20 +9,22 @@ public class AdministradorDTO extends UsuarioDTO
 	/**constructor 1 de la clase AdministradorDTO SIN CODIGO
 	 * @param correo
 	 * @param contraseña
+	 * @param estado
 	 */
-	public AdministradorDTO(String correo, String contraseña)
+	public AdministradorDTO(String correo, String contraseña, int estado)
 	{
-		super(correo, contraseña);
+		super(correo, contraseña, estado);
 	}
 	
 	/**constructor 2 de la clase AministradorDTO CON CODIGO
 	 * @param codigo
 	 * @param correo
 	 * @param contraseña
+	 * @param estado
 	 */
-	public AdministradorDTO(int codigo, String correo, String contraseña)
+	public AdministradorDTO(int codigo, String correo, String contraseña, int estado)
 	{
-		super(correo, contraseña);
+		super(correo, contraseña, estado);
 		this.codigo = codigo;
 	}
 }
