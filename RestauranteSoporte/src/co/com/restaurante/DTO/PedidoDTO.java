95608b831da2597195c71f7d694f211265e61a8e
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2019-09-17 12:57:31 -0500

    Update PedidoDTO.java

--- RestauranteSoporte/src/co/com/restaurante/DTO/PedidoDTO.java
+++ RestauranteSoporte/src/co/com/restaurante/DTO/PedidoDTO.java
@@ -7,6 +7,16 @@ package co.com.restaurante.DTO;
 public class PedidoDTO 
 {
 	/**
+	 * constante que representa es estado habilitado 
+	 */
+	public static final int ESTADO_HABILITADO = 1;
+	
+	/**
+	 * constante que representa es estado inhabilitado 
+	 */
+	public static final int ESTADO_INHABILITADO = 0;
+	
+	/**
 	 * atributo que representa el codigo PK
 	 */
 	private int codigo;
@@ -31,18 +41,25 @@ public class PedidoDTO
 	 */
 	private int totalProductos;
 	
+	/**
+	 * estado del pedido
+	 */
+	private int estado;
+	
 	/**construtor 1 de la clase PedidoDTO SIN CODIGO
 	 * @param codigoUsuario
 	 * @param codigoComedero
 	 * @param totalAPagar
 	 * @param totalProductos
+	 * @param estado
 	 */
-	public PedidoDTO(int codigoUsuario, int codigoComedero, Double totalAPagar, int totalProductos)
+	public PedidoDTO(int codigoUsuario, int codigoComedero, Double totalAPagar, int totalProductos, int estado)
 	{
 		this.codigoUsuario = codigoUsuario;
 		this.codigoComedero = codigoComedero;
 		this.totalAPagar = totalAPagar;
 		this.totalProductos = totalProductos;
+		this.estado = estado;
 	}
 	
 	/**construtor 1 de la clase PedidoDTO CODIGO CODIGO 
@@ -51,14 +68,16 @@ public class PedidoDTO
 	 * @param codigoComedero
 	 * @param totalAPagar
 	 * @param totalProductos
+	 * @param estado
 	 */
-	public PedidoDTO(int codigo, int codigoUsuario, int codigoComedero, Double totalAPagar, int totalProductos)
+	public PedidoDTO(int codigo, int codigoUsuario, int codigoComedero, Double totalAPagar, int totalProductos, int estado)
 	{
 		this.codigo = codigo;
 		this.codigoUsuario = codigoUsuario;
 		this.codigoComedero = codigoComedero;
 		this.totalAPagar = totalAPagar;
 		this.totalProductos = totalProductos;
+		this.estado = estado;
 	}
 
 	/**
@@ -129,6 +148,20 @@ public class PedidoDTO
 	 */
 	public void setTotalProductos(int totalProductos) {
 		this.totalProductos = totalProductos;
+	}
+
+	/**
+	 * @return estado
+	 */
+	public int getEstado() {
+		return estado;
+	}
+
+	/**
+	 * @param estado
+	 */
+	public void setEstado(int estado) {
+		this.estado = estado;
 	}
 	
 	
