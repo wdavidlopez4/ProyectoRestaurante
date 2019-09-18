932a8715ee6385a1c762d577a6f7e0f71c7e2f61
wdavidlopez4 <33738813+wdavidlopez4@users.noreply.github.com>
2019-09-18 13:48:01 -0500

    Update ComederoDTO.java

--- RestauranteSoporte/src/co/com/restaurante/DTO/ComederoDTO.java
+++ RestauranteSoporte/src/co/com/restaurante/DTO/ComederoDTO.java
@@ -7,6 +7,16 @@ package co.com.restaurante.DTO;
 public abstract class ComederoDTO 
 {
 	/**
+	 * constante que representa el estado habilitado
+	 */
+	public static final int ESTADO_HABILITADO =1;
+	
+	/**
+	 * constante que representa el estado inhabilitado
+	 */
+	public static final int ESTADO_INHABILITADO =0;
+	
+	/**
 	 * atributo que representa el codigo del comederoDTO
 	 */
 	protected int codigo;
@@ -22,12 +32,18 @@ public abstract class ComederoDTO
 	protected int posicion;
 	
 	/**
+	 * atributo que representa el estado
+	 */
+	protected int estado;
+	
+	/**
 	 * constructor 
 	 */
-	public ComederoDTO(int cantidadSillas, int posicion)
+	public ComederoDTO(int cantidadSillas, int posicion, int estado)
 	{
 		this.cantidadSillas = cantidadSillas;
 		this.posicion = posicion;
+		this.estado = estado;
 	}
 
 	/**
@@ -70,6 +86,20 @@ public abstract class ComederoDTO
 	 */
 	public void setPosicion(int posicion) {
 		this.posicion = posicion;
+	}
+
+	/**
+	 * @return estado
+	 */
+	public int getEstado() {
+		return estado;
+	}
+
+	/**
+	 * @param estado
+	 */
+	public void setEstado(int estado) {
+		this.estado = estado;
 	}
 	
 	
